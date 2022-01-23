ff9b3e8a7a1ac9f38dff12ab61919fa014745e55
bair0069 <bair0069@algonquinlive.com>
2022-01-23 14:53:40 -0500

    Add Array of songs

--- js/main.js
+++ js/main.js
@@ -6,12 +6,15 @@ import {audio} from './controls.js';
 audio.addEventListener('play', (ev)=>{
     console.log(audio.currentTime)
     toggleAnimation()
+    updateSongLength()
 })
 audio.addEventListener('pause',toggleAnimation)
 
 audio.addEventListener('durationchange',updateSongLength)
 
 audio.addEventListener('timeupdate',updateTime)
+
+
 
 
 
