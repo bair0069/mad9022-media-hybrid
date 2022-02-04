f2580dac98f0e33b867439597a63ea20167b2763
bair0069 <bair0069@algonquinlive.com>
2022-02-03 20:39:41 -0500

    Add more comments to understand the program

--- js/controls.js
+++ js/controls.js
@@ -1,20 +1,23 @@
-
 'use strict'
 import{stopAnimation, toggleAnimation, updateSongLength, updateTime, playlist,showPauseBtn, showPlayBtn, switchSong} from './visuals.js';
 import{songs}from "./songs.js";
+
+/* - - - - - - - - - - - - - - Global Variables - - - - - - - - - - - - - - */
+
 let songIndexes = songs.map((item) => {return item.title})
 let audio = document.getElementById('audio-player');
 let player = document.getElementById('player')
 let controls = document.getElementById('controls')
 let currentTrack = 0;
-let progressBar = document.getElementById('progress-bar')
 
 ;
 
 
-/* - - - - - - - - - - - - - - Controls - - - - - - - - - - - - - - */
+/* - - - - - - - - - - - - - - Event Listeners - - - - - - - - - - - - - - */
 
-window.addEventListener('keydown',(ev)=>{ // one event listener for the 
+//  - - - - - - - -Keyboard key listeners- - - - - 
+
+window.addEventListener('keydown',(ev)=>{ // listen for key presses in the window
     ev.preventDefault()
     let keyPushed = ev.key
     console.log(keyPushed)
@@ -33,35 +36,51 @@ window.addEventListener('keydown',(ev)=>{ // one event listener for the
     }
 })
 
+//  - - - - - - - -Control button listeners- - - - - 
+
 controls.addEventListener('click', (ev) => { // one event listener for all controls
     let target = ev.target;
     let btn = target.closest('button').getAttribute('id')
 //"btnSkipBack""btnReplay10""btnPlay""btnStop""btnFwd10""btnSkipForward"
     switch (btn) {
         case "btnSkipBack" :
-            previousSong()
+            previousSong()  // go back a song (defined on line 107)
         break;
-        case "btnReplay10" :
+        case "btnReplay10" : // Skip Backward 10 seconds
             back10()
         break;
-        case "btnPlay" :
-            playPause()
+        case "btnPlay" :    // Play selected song display pause or play depending on player classList
+            playPause()     // (defined on line 83)
         break;
-        case "btnStop" :
-            stopButton()
-        break;
+        case "btnStop" :    // 
+            stopButton()    // Stop art spinning, remove pause, display play, set audio current time to 0
+        break;              // (defined on line 95)
         case "btnFwd10" :
-            skip10()
+            skip10()        // Skip Forward 10 seconds
         break;
         case "btnSkipForward" :
-            nextSong()
+            nextSong() // go forward a song (defined on line 116)
         break;
     default:break;
     }
 })
 
 
-function playPause () { // Play selected song remove play button and display pause button
+//  - - - - - - - -Playlist Listeners- - - - -  
+
+
+playlist.addEventListener('click',(ev)=>{ // add eventListener to playlist items.
+    let target = ev.target;
+    let song = target.closest('.playlist-item')
+    let songTitle = song.getAttribute('data-title')
+    currentTrack = songIndexes.indexOf(songTitle)
+    switchSong(currentTrack)
+})
+
+//  - - - - - - - - - - - - - - Control Functions - - - - - - - - - - - - - - - - - - - -
+
+
+function playPause () { // Play selected song display pause or play depending on player classList
     if(player.classList.contains('is-playing')){
         audio.pause()
         showPlayBtn()
@@ -73,7 +92,7 @@ function playPause () { // Play selected song remove play button and display pau
 
 }
 
-function stopButton(){ // Stop art spinning, remove pause, display play, reload audio
+function stopButton(){ // Stop art spinning, remove pause, display play, set audio current time to 0
     player.classList.remove('is-playing')
     audio.pause()
     stopAnimation()
@@ -85,18 +104,17 @@ function skip10 () {audio.currentTime+=10}  // Skip forward 10 seconds
 
 function back10() {audio.currentTime-=10} // Skip Backward 10 seconds
 
-function previousSong(){
+function previousSong(){ // go back a song.
     if(currentTrack===0){}
     else{
         currentTrack-=1
-        switchSong(currentTrack)
+        switchSong(currentTrack)//update active item, and play new song (full comments in visuals.js line 32)
     }
     audio.currentTime=0;
     
 }
-function nextSong() {
+function nextSong() { // go forward a song
     console.log(songIndexes.length)
-            console.log(currentTrack)
             if(currentTrack===songIndexes.length-1){
                 console.log('end of playlist')
                 currentTrack=0
@@ -104,28 +122,26 @@ function nextSong() {
             else{
             currentTrack +=1}
             audio.currentTime=0; // reset time when switching songs
-            switchSong(currentTrack)
+            switchSong(currentTrack) //update active item, and play new song (full comments in visuals.js line 32)
 }
+
+
+
+//  - - - - - - - - - - - - - - - - - - - Playlist Creation - - - - - - - - - - - - 
+
+
 
 function createPlaylist(array) { // create song playlist in html from data in songs.js
-    playlist.textContent=""
-    array.forEach((item)=>{
-        let li = document.createElement('li')
-        li.setAttribute('data-src',item.src);
-        li.setAttribute('class', 'playlist-item')
-        li.setAttribute('data-title',item.title)
-        li.innerHTML = `<img src="${item.img}" alt="Cover art of ${item.title}"> <p>${item.title}</p> <p class="playlist-artist">${item.artist}</p>`
-        playlist.append(li)
+    playlist.textContent=""                         //clear playlist
+    array.forEach((item)=>{                         // for each item in songs create an li
+        let li = document.createElement('li')   
+        li.setAttribute('data-src',item.src);       // store the song source for extraction later
+        li.setAttribute('data-title',item.title)    // store the song title for extraction later
+        li.setAttribute('class', 'playlist-item')   // add the playlist-item class to the li
+        li.innerHTML = `<img src="${item.img}" alt="Cover art of ${item.title}"> <p>${item.title}</p> <p class="playlist-artist">${item.artist}</p>` 
+        playlist.append(li)                         // put the li into the playlist
     })
 }
-
-playlist.addEventListener('click',(ev)=>{ // add eventListener to playlist items.
-    let target = ev.target;
-    let song = target.closest('.playlist-item')
-    let songTitle = song.getAttribute('data-title')
-    currentTrack = songIndexes.indexOf(songTitle)
-    switchSong(currentTrack)
-})
 
 
 
--- js/visuals.js
+++ js/visuals.js
@@ -1,9 +1,9 @@
-
-import {playPause, player} from './controls.js'
+import {player} from './controls.js'
 import{songs}from "./songs.js";
 
-let playlist = document.getElementById('playlist')
+/* - - - - - - - - - - - - - - Global Variables - - - - - - - - - - - - - - */
 
+let playlist = document.getElementById('playlist')
 let playerArea = document.getElementById('player-area')
 let albumArt = document.getElementById('playing-song-art');
 let artist = document.getElementById('artist')
@@ -14,6 +14,11 @@ let playSpan = document.getElementById('play-span')
 let audio = document.getElementById('audio-player');
 
 
+/* - - - - - - - - - - - - - - Visual Functions - - - - - - - - - - - - - - */
+
+//  - - - - - Play/Pause display functions - - - - - -
+
+
 function showPlayBtn () { // display the play button
     playSpan.textContent='play_arrow';
 }
@@ -22,35 +27,36 @@ function showPauseBtn() { // display the pause button
 playSpan.textContent='pause';
 }
 
-function switchSong(index) {
-    player.classList.add('is-playing')
-    removeActiveTrack()
-    showPauseBtn()
+//  - - - - - Switch Song function - - - - - -
+
+function switchSong(index) { // update active item, and play new song.
+    player.classList.add('is-playing') // manually add 'is-playing' to player classlist
+    removeActiveTrack() // remove the active class from the "old" track
+    showPauseBtn() // display the pause button
     updateSongInfo(index)//update player background, song title, artist, and src
-    stopAnimation()
-    console.log(index)
-    
-    audio.play()
+    stopAnimation() // stop animation playing on the player
+    audio.play() // play the new song
 }
 
 
 /*  - - - - - - - update song information and player information - - - - -  */
 
 
-function removeActiveTrack() { // remove active from old current track
+function removeActiveTrack() {          // remove active from old current track
 let oldTrack = document.querySelector('.playlist-item.active')
 if(oldTrack){
     oldTrack.classList.remove('active')}
 }
 
-function secondsToMinutes (time) {  // convert time to seconds and minutes
-let minutes = Math.floor(time/60);
-let seconds = Math.round((time-minutes*60));
+function secondsToMinutes (time) {      // convert time to seconds and minutes
+let minutes = Math.floor(time/60); // divide seconds by 60
+let seconds = Math.round((time-minutes*60)); // subtract minutes from (se)
 let timeConverted= `${minutes}:${seconds.toString().padStart(2,'0')}`;
+console.log(seconds)
 return timeConverted
 }
 
-function updateTime () {  // update the amount of time that has passed in the song
+function updateTime () {                // update the amount of time that has passed in the song
     let progress = document.getElementById('progress')
     let timeLeft = secondsToMinutes(audio.currentTime)
     progressBar.setAttribute('data-currentTime',(timeLeft))
@@ -58,18 +64,21 @@ function updateTime () {  // update the amount of time that has passed in the so
     progress.setAttribute('max',audio.duration)
 }
 
-function updateSongLength () { //update the length of the song when songs change
+function updateSongLength () {          //update the length of the song when songs change
     progressBar.setAttribute('data-songLength',secondsToMinutes(audio.duration))
 }
 
+
 function updateSongInfo (index) { //update player background, song title, artist
-    playlistSongs[index].classList.add('active')    
-    audio.setAttribute('src',songs[index].src)
-    playerArea.style.backgroundImage=`linear-gradient(rgba(0, 0, 0, 0.705), rgba(0, 0, 0, 0.774)),url("${songs[index].img}")`
-    albumArt.setAttribute('src',songs[index].img);
-    song.textContent=`${songs[index].title}`;
-    artist.textContent=`${songs[index].artist}`
+    playlistSongs[index].classList.add('active')    // add active class to playlist-item
+    audio.setAttribute('src',songs[index].src)      // set audio.src to the new source
+    playerArea.style.backgroundImage=`linear-gradient(rgba(0, 0, 0, 0.705), rgba(0, 0, 0, 0.774)),url("${songs[index].img}")`                           // change player background image
+    albumArt.setAttribute('src',songs[index].img);  // change albumArt image in player
+    song.textContent=`${songs[index].title}`;       // change displayed song title in player
+    artist.textContent=`${songs[index].artist}`     // change displayed artist in player
 }
+
+//  - - - - - - - - - - - - - - Animation Controls - - - - - - - - - - - - -
 
 function stopAnimation () { // remove active class from albumArt
     albumArt.classList.remove('active')
