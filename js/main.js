301d0c4f95e62d72e45b2456300ffc60c1757386
bair0069 <bair0069@algonquinlive.com>
2022-03-19 10:05:05 -0400

    Add mute function/listener

--- js/main.js
+++ js/main.js
@@ -8,7 +8,12 @@ import{stopAnimation, toggleAnimation, updateSongInfo, updateSongLength, updateT
 /* - - - - - - - - - - - - - - - ******* TODO:********* - - - - - - - - - - - - - - - - - - - - - - - - - */
 /*  
 - - - - - - - - - - - Draggable progress bar   - - - - - - - - - - 
+- - - - - - - - - - - Add Volume Slider - - - - - - - 
+- - - - - - - - - - - Add volume_off, volume_mute Mute Button - - - - - - - 
+
 - - - - - - - - - - - Fix Volume Controls - - - - - - - 
+
+
 */
 function playSong(){
     toggleAnimation()
